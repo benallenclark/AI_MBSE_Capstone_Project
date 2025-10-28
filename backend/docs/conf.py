1211f8d84121d56709f184aef4bc1bad830318a1
benallenclark <ben@benallenclark.dev>
2025-10-28 17:13:17 -0500

    fix(docs): generate valid API toctree

--- backend/docs/conf.py
+++ backend/docs/conf.py
@@ -25,7 +25,13 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+# Avoid duplicate indexing of Context attributes
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+    "exclude-members": "model_id,vendor,version",
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
--- backend/docs/index.rst
+++ backend/docs/index.rst
@@ -15,6 +15,7 @@ documentation for details.
 
 .. toctree::
    :maxdepth: 2
+   :glob:
    :caption: API Reference
 
-   api/app
+   api/modules
